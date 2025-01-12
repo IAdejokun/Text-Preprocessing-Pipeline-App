f6297f70970a85a7ff61a625505645d39d385624
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2025-01-12 21:47:46 +0100

    route config initialised

---
+++ frontend/src/routes/all-routes.tsx
@@ -0,0 +1,39 @@
+
+import { lazy, Suspense } from 'react'
+import { BrowserRouter, Outlet, Route, Routes } from "react-router"
+import SkeletonLoader from '../components/skeleton-loader';
+
+
+const HomeComponent = lazy(() => import("../pages/home"));
+const ResultsComponent = lazy(() => import("../pages/results"));
+const PageNotFoundComponent = lazy(() => import("../pages/page-not-found"));
+const SignupComponent = lazy(() => import("../auth/signup-view"));
+const SigninComponent = lazy(() => import("../auth/signin-view"));
+
+const rootpath = "/";
+const homepath = "/home";
+const resultspath = "/results";
+const pagenotfoundpath = "*";
+const signuppath = "auth/signup";
+const signinpath = "auth/signin";
+
+
+const AllRoutesComponent = () => {
+  return (
+    <Suspense fallback={<SkeletonLoader />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={rootpath} element={<Outlet />} />
+          <Route index path={homepath} element={<HomeComponent/>} />
+          <Route path={resultspath} element={<ResultsComponent/>} />
+          <Route path={pagenotfoundpath} element={<PageNotFoundComponent/>} />
+          <Route path={signuppath} element={<SignupComponent/>} />
+          <Route path={signinpath} element={<SigninComponent/>} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
+  );
+}
+
+
+export default AllRoutesComponent;
