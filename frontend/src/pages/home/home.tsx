046d3305cb4fd5ba1d0a399f25f3c778086d3d06
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2025-01-14 18:02:15 +0100

    changes made to components

---
+++ frontend/src/pages/home/home.tsx
@@ -0,0 +1,14 @@
+import './home.module.css'
+
+
+const HomeComponent = () => {
+  return (
+    <>
+      <section>
+        <h2>Cleaning your data in a Jiffy</h2>
+    </section>
+    </>
+  )
+}
+
+export default HomeComponent;
