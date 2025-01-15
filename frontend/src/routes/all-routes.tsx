fc485b2a1fb16309402cdf4bf1710f4ea109edbd
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2025-01-15 14:58:33 +0100

    default route updated

--- frontend/src/routes/all-routes.tsx
+++ frontend/src/routes/all-routes.tsx
@@ -1,16 +1,16 @@
 
 import { lazy, Suspense } from 'react'
-import { BrowserRouter, Route, Routes } from "react-router"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router"
 import SkeletonLoader from '../components/skeleton-loader';
 
 
 const HomeComponent = lazy(() => import("../pages/home/home"));
 const ResultsComponent = lazy(() => import("../pages/results"));
 const PageNotFoundComponent = lazy(() => import("../pages/page-not-found"));
-const SignupComponent = lazy(() => import("../auth/signup-view"));
-const SigninComponent = lazy(() => import("../auth/signin-view"));
+const SignupComponent = lazy(() => import("../auth/signup/signup-view"));
+const SigninComponent = lazy(() => import("../auth/signin/signin-view"));
 
-// const rootpath = "/";
+const rootpath = "/";
 const homepath = "/home";
 const resultspath = "/results";
 const pagenotfoundpath = "*";
@@ -23,8 +23,8 @@ const AllRoutesComponent = () => {
     <Suspense fallback={<SkeletonLoader />}>
       <BrowserRouter>
         <Routes>
-          {/* <Route path={rootpath} element={<Outlet />} /> */}
-          <Route index path={homepath} element={<HomeComponent/>} />
+          <Route path={rootpath} element={<Navigate to={signinpath} replace />} />
+          <Route path={homepath} element={<HomeComponent/>} />
           <Route path={resultspath} element={<ResultsComponent/>} />
           <Route path={signuppath} element={<SignupComponent/>} />
           <Route path={signinpath} element={<SigninComponent/>} />
